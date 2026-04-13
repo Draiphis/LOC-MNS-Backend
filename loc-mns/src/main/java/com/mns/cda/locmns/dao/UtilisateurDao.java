d17090709c0416d5c78cb2c170fc822bac1f5f96
Draiphis <val08350@gmail.com>
2026-04-13 15:43:23 +0200

    PATCH : ajout extension JPA repository

--- loc-mns/src/main/java/com/mns/cda/locmns/dao/UtilisateurDao.java
+++ loc-mns/src/main/java/com/mns/cda/locmns/dao/UtilisateurDao.java
@@ -1,8 +1,10 @@
 package com.mns.cda.locmns.dao;
 
 
+import com.mns.cda.locmns.model.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UtilisateurDao {
+public interface UtilisateurDao extends JpaRepository<Utilisateur, Integer> {
 }
