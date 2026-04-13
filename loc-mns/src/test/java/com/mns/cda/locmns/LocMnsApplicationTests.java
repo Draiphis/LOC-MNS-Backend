507488c65b7a5a24ddd5a4c926b9c23df6ac58ea
Draiphis <val08350@gmail.com>
2026-04-13 11:00:44 +0200

    CREATE : initialisation du projet

---
+++ loc-mns/src/test/java/com/mns/cda/locmns/LocMnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mns.cda.locmns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocMnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
