c61835d65d9ae8cb60406bcf242371f5b2cb7fc9
Draiphis <val08350@gmail.com>
2026-04-14 11:19:24 +0200

    PATCH : ajout des décorateurs a la classe Etat

--- loc-mns/src/main/java/com/mns/cda/locmns/model/Etat.java
+++ loc-mns/src/main/java/com/mns/cda/locmns/model/Etat.java
@@ -2,7 +2,16 @@ package com.mns.cda.locmns.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Etat {
 
     @Id
