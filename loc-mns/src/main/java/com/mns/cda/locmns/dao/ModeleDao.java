b8b905a4f8b506965a1b170e23dcde0189c58334
Draiphis <val08350@gmail.com>
2026-04-15 11:16:21 +0200

    CREATE : ajout du dao Modele

---
+++ loc-mns/src/main/java/com/mns/cda/locmns/dao/ModeleDao.java
@@ -0,0 +1,10 @@
+package com.mns.cda.locmns.dao;
+
+
+import com.mns.cda.locmns.model.Modele;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModeleDao extends JpaRepository<Modele, Integer> {
+}
