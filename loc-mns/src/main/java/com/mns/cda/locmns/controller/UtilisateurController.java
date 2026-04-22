97520433c91c459bb0398b5d89a2d96ab470a6b9
Draiphis <val08350@gmail.com>
2026-04-22 15:51:45 +0200

    fix (utilisateur) : récupération de tout les utilisateurs fonctionnelle

--- loc-mns/src/main/java/com/mns/cda/locmns/controller/UtilisateurController.java
+++ loc-mns/src/main/java/com/mns/cda/locmns/controller/UtilisateurController.java
@@ -3,9 +3,11 @@ package com.mns.cda.locmns.controller;
 
 import com.mns.cda.locmns.dao.UtilisateurDao;
 import com.mns.cda.locmns.model.Utilisateur;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,9 @@ public class UtilisateurController {
     private final UtilisateurDao utilisateur;
 
 
+    @GetMapping("/list")
+    @Operation(summary="Récupère tout les utilisateurs",
+            description = "Récupère et affiche la liste de tout les utilisateurs et leurs données associés")
     public List<Utilisateur> getAll(){
 
         return utilisateur.findAll();
