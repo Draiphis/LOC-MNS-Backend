1d2c2ca0c7d39385b55a695431ccfca51b5ebd17
Draiphis <val08350@gmail.com>
2026-04-23 16:53:07 +0200

    feat (utilisateur) : creation du dto pour la modification d'utilisateur

---
+++ loc-mns/src/main/java/com/mns/cda/locmns/dto/UpdateUtilisateurDto.java
@@ -0,0 +1,30 @@
+package com.mns.cda.locmns.dto;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class UpdateUtilisateurDto {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 36)
+    private String password;
+
+    @NotBlank
+    private String nom;
+
+    @NotBlank
+    private String prenom;
+
+    @NotNull
+    @Past
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dateDeNaissance;
+}
