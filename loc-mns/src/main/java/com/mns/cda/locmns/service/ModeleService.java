eb66df7a5789a578d63d34bf5718d5a19455ac15
Draiphis <val08350@gmail.com>
2026-04-27 14:13:43 +0200

    feat (modele) : creation du service pour la classe modele

---
+++ loc-mns/src/main/java/com/mns/cda/locmns/service/ModeleService.java
@@ -0,0 +1,52 @@
+package com.mns.cda.locmns.service;
+
+import com.mns.cda.locmns.dao.ModeleDao;
+import com.mns.cda.locmns.dto.CreateModeleDto;
+import com.mns.cda.locmns.dto.UpdateModeleDto;
+import com.mns.cda.locmns.model.Modele;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ModeleService {
+    private final ModeleDao modeleDao;
+
+    // CREATE
+    public Modele create(CreateModeleDto dto) {
+        Modele u = new Modele();
+        
+        u.setNom(dto.getNom());
+        
+
+        return modeleDao.save(u);
+    }
+
+    // UPDATE (PUT)
+    public void update(int id, UpdateModeleDto dto) {
+        Modele u = modeleDao.findById(id)
+                .orElseThrow(() -> new RuntimeException("Modele non trouvé"));
+        
+        u.setNom(dto.getNom());
+        modeleDao.save(u);
+    }
+
+    // DELETE
+    public void delete(int id) {
+        if (!modeleDao.existsById(id)) {
+            throw new RuntimeException("Modele non trouvé");
+        }
+        modeleDao.deleteById(id);
+    }
+
+    // GET ONE
+    public Modele getById(int id) {
+        return modeleDao.findById(id)
+                .orElseThrow(() -> new RuntimeException("Modele non trouvé"));
+    }
+
+    // GET ALL
+    public java.util.List<Modele> getAll() {
+        return modeleDao.findAll();
+    }
+}
