5dda906ef09cb642d13f5a72cbaadb1d256ea68e
Draiphis <val08350@gmail.com>
2026-05-15 19:34:37 +0200

    feat (marque) : ajout des JsonView

--- loc-mns/src/main/java/com/mns/cda/locmns/model/Marque.java
+++ loc-mns/src/main/java/com/mns/cda/locmns/model/Marque.java
@@ -1,5 +1,9 @@
 package com.mns.cda.locmns.model;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.mns.cda.locmns.view.MarqueView;
+import com.mns.cda.locmns.view.ModeleView;
+import com.mns.cda.locmns.view.UtilisateurView;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -18,10 +22,12 @@ import java.util.Set;
 public class Marque {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonView({MarqueView.class, ModeleView.class} )
     protected Integer id;
 
     @Column(nullable = false)
     @NotBlank(message = "Le nom ne peut pas être vide")
+    @JsonView({MarqueView.class,ModeleView.class} )
     protected String nom;
 
     @OneToMany(mappedBy = "marque")
