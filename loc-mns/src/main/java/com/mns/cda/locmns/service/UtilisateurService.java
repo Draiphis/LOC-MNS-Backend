a22073cbb4396cd3e285b4fbe0dc60d6d1888d1c
Draiphis <val08350@gmail.com>
2026-05-15 14:44:06 +0200

    feat (utilisateur) : ajout d'un role par defaut

--- loc-mns/src/main/java/com/mns/cda/locmns/service/UtilisateurService.java
+++ loc-mns/src/main/java/com/mns/cda/locmns/service/UtilisateurService.java
@@ -1,16 +1,22 @@
 package com.mns.cda.locmns.service;
 
+import com.mns.cda.locmns.dao.RoleDao;
 import com.mns.cda.locmns.dao.UtilisateurDao;
 import com.mns.cda.locmns.dto.CreateUtilisateurDto;
 import com.mns.cda.locmns.dto.UpdateUtilisateurDto;
+import com.mns.cda.locmns.model.Role;
+import com.mns.cda.locmns.model.RoleNom;
 import com.mns.cda.locmns.model.Utilisateur;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class UtilisateurService {
     private final UtilisateurDao utilisateurDao;
+    private final RoleDao roleDao;
 
     // CREATE
     public Utilisateur create(CreateUtilisateurDto dto) {
@@ -21,6 +27,12 @@ public class UtilisateurService {
         u.setNom(dto.getNom());
         u.setPrenom(dto.getPrenom());
         u.setDateDeNaissance(dto.getDateDeNaissance());
+
+        Role defaultRole = roleDao.findByRole(RoleNom.DEFAULT)
+                .orElseThrow(() -> new RuntimeException("Role DEFAULT introuvable"));
+
+
+        u.setRoles(Set.of(defaultRole));
 
         return utilisateurDao.save(u);
     }
